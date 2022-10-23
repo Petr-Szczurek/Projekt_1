6b979e04f99333d7009a6b6362d8c1a5021c862a
Petr Szczurek <petr.szczurek1@gmail.com>
2022-10-23 23:55:47 +0200

    vstup souboru + zacatek ukladani do pole

--- t9search.c
+++ t9search.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#define radek 84
+#define sloupec 100
 
 int main ()
 {
-	printf("Hello, World!\n";
+	// plneni kontaktu do pole
+	int seznam [radek][sloupec];
+	char znak = 'a';
+	int row = 0;
+	int col = 0;
+	while (znak != EOF)
+	{
+		znak = getchar ();
+		if (znak == EOF)
+		{
+			break;
+		}
+		if (znak == '\0') // posouva na dalsi radek
+		{
+			row++;
+			col = 0; // reset sloupcu
+		}
+		seznam [row] [col] = znak; // zapis
+
+		col++; // posouva na dalsi sloupec v poli
+		printf ("%c", znak);
+	}
+
+	printf ("\n");
+
+	int j;
+	for (int i = 0; i < 6; i++)
+	{
+		for (j = 0; j < 19; j++)
+		{
+			printf ("%d", seznam [i][j]);
+		}
+	}
+	
 	return 0;
 }
