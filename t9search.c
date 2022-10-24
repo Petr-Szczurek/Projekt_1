f837a4a25eec672a31ef5119b2598bab43a8c327
Petr Szczurek <petr.szczurek1@gmail.com>
2022-10-24 15:35:25 +0200

    kontrola vstupu + ulozeni vstupu do pole

--- t9search.c
+++ t9search.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
-#define radek 84
-#define sloupec 100
 
-int main ()
+int main (int argc, char *argv[])
 {
-	// plneni kontaktu do pole
-	int seznam [radek][sloupec];
-	char znak = 'a';
-	int row = 0;
-	int col = 0;
-	while (znak != EOF)
-	{
-		znak = getchar ();
-		if (znak == EOF)
-		{
-			break;
-		}
-		if (znak == '\0') // posouva na dalsi radek
-		{
-			row++;
-			col = 0; // reset sloupcu
-		}
-		seznam [row] [col] = znak; // zapis
-
-		col++; // posouva na dalsi sloupec v poli
-		printf ("%c", znak);
-	}
-
-	printf ("\n");
-
-	int j;
-	for (int i = 0; i < 6; i++)
-	{
-		for (j = 0; j < 19; j++)
-		{
-			printf ("%d", seznam [i][j]);
-		}
-	}
 	
+	// kontrola zadaneho cisla
+	char znak = 'a';
+	// printf ("%d", argc); //pocet argumentu jsou 2 pokud zadam cislo
+	if (argc < 2) 
+    {
+		while (znak != EOF) // vytiskne seznam pokud uzivatel nezada zadne cislo
+		{
+			znak = getchar ();
+			if (znak == EOF)
+			{
+				break;
+			}
+			printf ("%c", znak);
+		}
+		printf ("\n");
+    }
+	else if (argc == 2)
+	{
+		// zapis vstupu od uzivatele do pole (max 100 znaku)
+		int i = 0;
+		char vstupni_znak = 'a';
+		char char_vstup [100];
+		while (vstupni_znak != '\0')
+		{
+			if (argv[1][i] <= '0' || argv[1][i] >= '9') //kontroluje zda se jedna o 0-9 znaky
+			{
+				printf ("Spatne zadany vstup - jine nez 0-9\n");
+				return 1;
+			}
+			if (i == 100) // kontroluje pocet zadanych znaku
+			{
+				printf ("\n");
+				printf ("Prekrocena hranice cisel - max 100\n");
+				return 1;
+			}
+			
+			char_vstup [i] = argv[1][i];
+			// printf ("%c", char_vstup [i]);
+			i++;
+			vstupni_znak = argv[1][i];
+		}
+		// printf ("\n");
+
+		while (znak != EOF) // porovnani vsech znaku
+		{
+			znak = getchar ();
+			if (znak == EOF)
+			{
+				break;
+			}
+		}
+		
+
+	}
+	else
+	{
+		printf ("Spatne zadany vstup\n");
+	}
+
 	return 0;
 }
